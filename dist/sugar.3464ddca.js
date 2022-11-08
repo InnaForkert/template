dd281f298d34b705f2bb5e01aebf01a1c968fe80
Inna Forkert <i.forkert@med.sumdu.edu.ua>
2022-11-08 18:20:17 +0200

    renamed

--- dist/sugar.3464ddca.js
+++ dist/sugar.3464ddca.js
@@ -533,6 +533,6 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"j4kuM":[function(require,module,exports) {
 
-},{}]},["c4xoB","j4kuM"], "j4kuM", "parcelRequire9ed0")
+},{}]},["c4xoB","j4kuM"], "j4kuM", "parcelRequirea579")
 
 //# sourceMappingURL=sugar.3464ddca.js.map
